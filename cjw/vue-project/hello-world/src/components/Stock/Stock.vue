497beaa586d532b4f3bd4e8bc8343a8aa3ef3f4f
penny <penny880905@gmail.com>
2020-07-24 15:56:35 +0800

    0000000

---
+++ cjw/vue-project/hello-world/src/components/Stock/Stock.vue
@@ -0,0 +1,23 @@
+
+<template>
+    <div>
+        <input v-model='newstock' @keyup.enter='addlist()' placeholder="輸入文字" >
+        <button @click='addlist()'>add</button>
+        <hr>
+        <search-stock-list :list='searchStockList'></search-stock-list>
+        <hr>
+        <stock-table 
+            :years='years'
+            :earn_values='stockData.earn_values'>
+        </stock-table>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'Stock',
+   
+}
+</script>
