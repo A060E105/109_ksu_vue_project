0e1e772765c4ed29327c80220bfa4d3d1edb3976
e3051945 <ss950701@gmail.com>
2020-07-24 11:02:54 +0800

    single file

---
+++ 3_quiznick/hello-world/src/components/todolist.vue
@@ -0,0 +1,74 @@
+<template>
+
+  <div>
+    <textinput
+    v-model="newtodotxt"
+    placeholder="請輸入文字"
+    ＠keyup.enter="addtodo" 
+    />
+    <ol v-if="todos.lists">
+      <todoitem
+      v-for="todp in todos "
+      :key="todo.id"
+      :todo="todo"
+      @remove="removetodo"
+      />
+    </ol>
+
+      <p v-else>
+        請新增備忘錄
+        </p>
+
+    </div>
+
+  </template>
+
+  <script>
+    import textinput from './textinput.vue'
+    import todoitem from './todoitem.vue'
+
+    let nextid =1 
+    export default{
+      components:{
+        textinput,todoitem
+
+      },
+      data(){
+        return{
+        newtodotxt:'',
+        todos:[
+
+        ]
+
+        }
+      }
+
+  
+    }
+    methods:{
+      addtodo() 
+      {
+        const cutext=this.cutext.trim()
+        if (cutext){
+          this.todos.push({
+
+           id:nextid++,
+           text:cutext
+
+          })
+          this.newtodotxt=''
+        }
+
+      }
+      removetodo(idtoremove)
+      {
+       this.todos=this.todos.filter(todo =>{
+         return todo.id   !== idtoremove
+       })
+      }
+      
+
+    }
+
+
+    </script>
---
+++ 3_quiznick/hello-world/src/components/todoltem.vue
@@ -0,0 +1,23 @@
+<template>
+<ol>
+
+  <button @click="$emit('remove',todo.id)">
+    刪除
+    </button>
+  </ol>
+
+</template>
+
+<script>
+export default {
+props:{
+  todo:{
+    type:Object,
+    required:true  
+  }
+
+}
+
+
+}
+</script>
