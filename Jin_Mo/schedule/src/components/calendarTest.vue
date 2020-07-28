e898ecafe6d459a50728d939c29577b63d0753a6
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-07-29 00:13:42 +0800

    test: calendar event and locale

--- Jin_Mo/schedule/src/components/calendarTest.vue
+++ Jin_Mo/schedule/src/components/calendarTest.vue
@@ -2,15 +2,17 @@
     <div>
         <FullCalendar 
             :options="calendarOptions" 
-            locale='zh-cn' />
+            ref="calendar"
+        />
     </div>
 </template>
 
 
 <script>
-import FullCalendar from '@fullcalendar/vue'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
+import FullCalendar from '@fullcalendar/vue';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import tw from '@fullcalendar/core/locales/zh-tw';
 
 export default {
     name: 'calendarTest',
@@ -19,11 +21,40 @@ export default {
     },
     data() {
         return {
-        calendarOptions: {
-            plugins: [ dayGridPlugin, interactionPlugin ],
-            initialView: 'dayGridMonth'
+            calendarOptions: {
+                locale: tw,
+                plugins: [ dayGridPlugin, interactionPlugin ],
+                initialView: 'dayGridMonth',
+                dateClick: this.handleDateClick,
+                eventClick: this.eventClick,
+                fixedWeekCount: false,
+                events: [
+                    { title: 'event 1', date: '2020-07-28' },
+                    { title: 'event 2', date: '2020-07-28' },
+                    { title: 'event 3', date: '2020-07-28' },
+                    { title: 'event 4', date: '2020-07-29' }
+                ],
+            }
         }
-        }
-    }
+    },
+    mounted() {
+        let calendarApi = this.$refs.calendar.getApi();
+        console.log(calendarApi.getEvents());
+    },
+    methods: {
+        handleDateClick: function(arg) {
+            // alert('date click! ' + arg.dateStr);
+            console.log(arg.dateStr);
+        },
+        eventClick: function(event, jsEvent, pos) {
+            console.log('eventClick', event, jsEvent, pos);
+            event.event.remove();
+      },
+  }
 }
 </script>
+
+<style>
+/* @import '@fullcalendar/core/main.css'; */
+/* @import '@fullcalendar/daygrid/main.css'; */
+</style>
