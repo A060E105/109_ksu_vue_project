45209b68b1f67b2d99c29dc2d68ebfe5b8f5ad7d
yuyu7708 <s106000586@g.ksu.edu.tw>
2020-08-10 17:22:01 +0800

    create package:holiday

---
+++ jianlin/project/src/components/Calendar .vue
@@ -0,0 +1,28 @@
+<template>
+    <tr>
+      <td>{{ list.week }}</td>
+      <td @click='holidayChecked(0)' ></td>
+      <td @click='holidayChecked(1)' ></td>
+      <td @click='holidayChecked(2)' ></td>
+      <td @click='holidayChecked(3)' ></td>
+      <td @click='holidayChecked(4)' ></td>
+      <td @click='holidayChecked(5)' ></td>
+      <td @click='holidayChecked(6)' ></td>
+    </tr>
+</template>
+<script>
+  export default {
+    name:'Calendar',
+    props:[
+      'list',
+      'buttonType'
+    ],
+    methods:{
+      holidayChecked(days)
+      {
+        this.tableData[this.buttonType]=days;
+        console.log(this.tableData);
+      }
+    }
+  }
+</script>
---
+++ jianlin/project/src/components/holiday.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="btn-group" role="group">
+        <button :buttonType="'leave'" type="button" class="btn btn-secondary">休假</button>
+        <button :buttonType="'Legal'" type="button" class="btn btn-secondary" >例假</button>
+    </div>
+    <table class="table"
+      ref="singleTable"
+      :data="tableData"
+      highlight-current-row
+      style="width: 100%">
+      <thead>
+        <tr>
+          <th property="week">週次</th>
+          <th property="week">星期日</th>
+          <th property="week">星期一</th>
+          <th property="week">星期二</th>
+          <th property="week">星期三</th>
+          <th property="week">星期四</th>
+          <th property="week">星期五</th>
+          <th property="week">星期六</th>
+        </tr>
+      </thead>
+      <tr is='Calendar' 
+        v-for="lists in tableData"
+        :list='lists'
+        :key="lists.week"
+      />
+    </table>
+
+  </div>
+</template>
+<script>
+import Calendar from './Calendar ';
+export default {
+  name:'holiday',
+  components:{
+    Calendar
+  },
+  data(){
+    return {
+      buttonType:'leave',
+      tableData:[
+           {week:'1',legal:'',leave:''},
+           {week:'2',legal:'',leave:''},
+           {week:'3',legal:'',leave:''},
+           {week:'4',legal:'',leave:''},
+           {week:'5',legal:'',leave:''},
+           {week:'6',legal:'',leave:''},
+         ]
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
