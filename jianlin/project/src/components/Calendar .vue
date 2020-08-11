d08229a5b786f4228a6832edbdc3a8f01e8418c9
yuyu7708 <s106000586@g.ksu.edu.tw>
2020-08-11 22:47:51 +0800

    update:component（holiday）

--- jianlin/project/src/components/Calendar .vue
+++ jianlin/project/src/components/Calendar .vue
@@ -1,13 +1,13 @@
 <template>
     <tr>
       <td>{{ list.week }}</td>
-      <td @click='holidayChecked(0)' ></td>
-      <td @click='holidayChecked(1)' ></td>
-      <td @click='holidayChecked(2)' ></td>
-      <td @click='holidayChecked(3)' ></td>
-      <td @click='holidayChecked(4)' ></td>
-      <td @click='holidayChecked(5)' ></td>
-      <td @click='holidayChecked(6)' ></td>
+      <td @click='holidayChecked(0)' :class="[list.legal==0 ? legalClass : list.leave==0 ? leaveClass :null]"></td>
+      <td @click='holidayChecked(1)' :class="[list.legal==1 ? legalClass : list.leave==1 ? leaveClass :null]"></td>
+      <td @click='holidayChecked(2)' :class="[list.legal==2 ? legalClass : list.leave==2 ? leaveClass :null]"></td>
+      <td @click='holidayChecked(3)' :class="[list.legal==3 ? legalClass : list.leave==3 ? leaveClass :null]"></td>
+      <td @click='holidayChecked(4)' :class="[list.legal==4 ? legalClass : list.leave==4 ? leaveClass :null]"></td>
+      <td @click='holidayChecked(5)' :class="[list.legal==5 ? legalClass : list.leave==5 ? leaveClass :null]"></td>
+      <td @click='holidayChecked(6)' :class="[list.legal==6 ? legalClass : list.leave==6 ? leaveClass :null]"></td>
     </tr>
 </template>
 <script>
@@ -15,14 +15,32 @@
     name:'Calendar',
     props:[
       'list',
-      'buttonType'
+      'btnType'
     ],
+    data(){
+      return {
+        legalClass: 'legal',
+        leaveClass: 'leave'
+      }
+    },
     methods:{
-      holidayChecked(days)
-      {
-        this.tableData[this.buttonType]=days;
-        console.log(this.tableData);
+      holidayChecked(days){
+        if(this.btnType!='leave'){
+          this.list.legal==days ? this.list.legal=null : this.list.legal=days;
+        }
+        else{
+          this.list.leave==days ? this.list.leave=null : this.list.leave=days;
+        }
+        console.log(this.list);
       }
     }
   }
 </script>
+<style>
+  .legal{
+    background-color: #f0a76c;
+  }
+  .leave{
+    background-color: #7ceb84;
+  }
+</style>
--- jianlin/project/src/components/holiday.vue
+++ jianlin/project/src/components/holiday.vue
@@ -1,33 +1,31 @@
 <template>
   <div>
     <div class="btn-group" role="group">
-        <button :buttonType="'leave'" type="button" class="btn btn-secondary">休假</button>
-        <button :buttonType="'Legal'" type="button" class="btn btn-secondary" >例假</button>
+        <button @click="buttonType='leave'" type="button" class="btn btn-primary">休假</button>
+        <button @click="buttonType='Legal'" type="button" class="btn btn-primary" >例假</button>
     </div>
-    <table class="table"
-      ref="singleTable"
-      :data="tableData"
-      highlight-current-row
-      style="width: 100%">
+    <table class="table table-bordered">
       <thead>
         <tr>
-          <th property="week">週次</th>
-          <th property="week">星期日</th>
-          <th property="week">星期一</th>
-          <th property="week">星期二</th>
-          <th property="week">星期三</th>
-          <th property="week">星期四</th>
-          <th property="week">星期五</th>
-          <th property="week">星期六</th>
+            <th scope="col">週次</th>
+            <th scope="col">星期日</th>
+            <th scope="col">星期一</th>
+            <th scope="col">星期二</th>
+            <th scope="col">星期三</th>
+            <th scope="col">星期四</th>
+            <th scope="col">星期五</th>
+            <th scope="col">星期六</th>
         </tr>
       </thead>
-      <tr is='Calendar' 
-        v-for="lists in tableData"
-        :list='lists'
-        :key="lists.week"
-      />
+      <tbody>
+        <tr is='Calendar' 
+          v-for="lists in tableData"
+          :list='lists'
+          :btnType='buttonType'
+          :key="lists.week"
+        />
+      </tbody>
     </table>
-
   </div>
 </template>
 <script>
@@ -41,12 +39,12 @@ export default {
     return {
       buttonType:'leave',
       tableData:[
-           {week:'1',legal:'',leave:''},
-           {week:'2',legal:'',leave:''},
-           {week:'3',legal:'',leave:''},
-           {week:'4',legal:'',leave:''},
-           {week:'5',legal:'',leave:''},
-           {week:'6',legal:'',leave:''},
+           {week:'1',legal:null,leave:null},
+           {week:'2',legal:null,leave:null},
+           {week:'3',legal:null,leave:null},
+           {week:'4',legal:null,leave:null},
+           {week:'5',legal:null,leave:null},
+           {week:'6',legal:null,leave:null},
          ]
     }
   }
